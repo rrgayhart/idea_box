bd5a117d36baf554ea05e0f189969ecf8863125d
Meeka <rrgayhart@gmail.com>
2013-10-17 11:13:16 -0600

    impliments the like feature

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,11 +1,27 @@
 require 'yaml/store'
 
 class Idea
-  attr_reader :title, :description
+  attr_reader :title, :description, :rank
   
   def initialize(attributes = {})
     @title = attributes["title"]
     @description = attributes["description"]
+    @rank = attributes["rank"] || 0
   end
 
+  def save
+    IdeaStore.create(to_h)
+  end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => rank
+    }
+  end
+
+  def like!
+    @rank += 1
+  end
 end
