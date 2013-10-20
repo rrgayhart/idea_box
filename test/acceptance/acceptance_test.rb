b5db50c2a3d5554d818e42f0f4a7626935918631
Meeka <rrgayhart@gmail.com>
2013-10-20 16:43:21 -0600

    Adds further capybara testing

--- test/acceptance/acceptance_test.rb
+++ test/acceptance/acceptance_test.rb
@@ -14,6 +14,19 @@ class UserAcceptanceTest < Minitest::Test
     assert page.has_content?("Welcome")
   end
 
-  # write your tests here
+  def test_manage_ideas
+    visit '/'
+    fill_in 'idea[title]', :with => 'eat'
+    fill_in 'idea[description]', :with => 'chocolate chip cookies'
+    click_button 'Save'
+    assert page.has_content?("chocolate chip cookies"), "Idea is not on page"
+  end
+
+  def test_manage_idea_view
+    idea = Idea.new({"title" => "run", "description" => "at least one mile per day", "rank" => 1})
+    idea.save
+    visit '/0'
+    assert page.has_content?("run")
+  end
 
 end
