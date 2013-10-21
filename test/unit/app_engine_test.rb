1b00a5c1b0773219bc8b515ee6f1df2f330dcb9c
Meeka <rrgayhart@gmail.com>
2013-10-21 17:28:05 -0600

    Changes to CSS

--- test/unit/app_engine_test.rb
+++ test/unit/app_engine_test.rb
@@ -22,7 +22,7 @@ class AppEngineTest < Minitest::Test
     get '/'
     assert last_response.ok?
     assert_equal 200, last_response.status
-    assert last_response.body =~ /Welcome/
+    assert last_response.body =~ /Existing/
   end
 
   def test_create_new_idea
